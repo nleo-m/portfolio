4433a44115ad75175fa01cf114e70e9007632d2f
Leonel Marcos <mfjleonel@gmail.com>
2024-07-02 15:38:23 -0300

    Initial commit

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    main: {},
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "#181818",
+      },
+    },
+  },
+});
+
+export default theme;
