5277cb43442598e9aa6fba06e8f03fd523ded701
Leonel Marcos <mfjleonel@gmail.com>
2024-10-02 09:58:21 -0300

    feat: add responsivity to landing section

--- src/sections/LandingSection/index.jsx
+++ src/sections/LandingSection/index.jsx
@@ -1,11 +1,24 @@
-import { Button, Center, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import SpinningPlanet from "@/components/SpinningPlanet";
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
 import { scrollToAnchor } from "@/utils/scrollToAnchor";
 
 export default function LandingSection() {
+  const [isMobile] = useMediaQuery("(max-width: 480px)", { ssr: false });
   const { t } = useTranslation();
+
+  useEffect(() => {
+    console.log(isMobile);
+  }, [isMobile]);
 
   const occupations = [
     t("full_dev"),
@@ -61,16 +74,22 @@ export default function LandingSection() {
       <Flex position="relative" align="center" w="80%">
         <Flex
           direction="column"
-          w="80%"
+          w={"80%"}
           position="relative"
           gap="2px"
           zIndex={1}
         >
-          <Heading fontSize={38} mb="12px">
+          <Heading fontSize={{ base: 18, md: 32, lg: 38 }} mb="12px">
             Hello world!
           </Heading>
-          <Heading as="h2" fontSize={42} mb=".75em">
+          <Heading
+            as="h2"
+            fontSize={{ base: 24, md: 36, lg: 42 }}
+            mb=".75em"
+            whiteSpace={{ base: "pre-line", md: "normal" }}
+          >
             {t("i_am")}
+            {"\n"}
             {occupationRender}
           </Heading>
           <Text mb="12px">
@@ -111,35 +130,48 @@ export default function LandingSection() {
           <Text>Sinta-se à vontade para entrar em contato comigo :{")"}</Text>
 
           <Button
-            mt="75px"
+            mt={{ base: "32px", md: "64px" }}
             borderRadius="2px"
-            paddingY="28px"
-            paddingX="64px"
+            paddingY={{ base: "22px", md: "28px" }}
+            paddingX={{ base: "16px", md: "64px" }}
             w="fit-content"
             bg="main.green"
             transition=".1s ease"
             _hover={{ bg: "main.lightGreen" }}
             onClick={() => scrollToAnchor("contact")}
           >
-            Clique aqui para me mandar uma mensagem
+            Mande uma mensagem
           </Button>
         </Flex>
 
         <Flex
           position="absolute"
-          w="40%"
+          w={{ base: "75%", sm: "60%", lg: "40%" }}
           h="100%"
           align="center"
-          right={-10}
+          right={{ base: -30, md: -75, lg: -10 }}
           zIndex={1}
+          overflow={{ base: "hidden", lg: "visible" }}
         >
-          <Flex position="absolute" w="620px" h="620px">
+          <Flex
+            position="absolute"
+            top={{ base: -50, md: "auto" }}
+            w={{ base: "520px", md: "620px" }}
+            h={{ base: "520px", md: "620px" }}
+          >
             <SpinningPlanet />
           </Flex>
 
-          <Text position="absolute" top="5%" right="-24px" color="#B2D5CE">
-            Hack the planet
-          </Text>
+          {!isMobile && (
+            <Text
+              position="absolute"
+              top="5%"
+              right={{ sm: "24px", lg: "-24px" }}
+              color="#B2D5CE"
+            >
+              Hack the planet
+            </Text>
+          )}
         </Flex>
       </Flex>
     </Center>
