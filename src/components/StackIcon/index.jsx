5f95555788570f44819c6737f3cbe383c0fc831f
Leonel Marcos <mfjleonel@gmail.com>
2024-10-08 12:48:46 -0300

    feat: add StackIcon component

---
+++ src/components/StackIcon/index.jsx
@@ -0,0 +1,28 @@
+import { Icon } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+
+export default function StackIcon(props) {
+  const { color } = useSelector((state) => state.theme);
+
+  const { children, hoverFill = "blue.500", invert = false } = props;
+
+  return (
+    <Icon
+      viewBox="0 0 32 32"
+      boxSize={20}
+      transition=".25s ease"
+      fill={invert ? "none" : `main.${color}`}
+      background={invert ? `main.${color}` : "none"}
+      borderRadius="2px"
+      _hover={{
+        cursor: "pointer",
+        transform: "scale(1.10)",
+        ...(!invert && { fill: hoverFill }),
+        ...(invert && { background: hoverFill }),
+      }}
+      {...props}
+    >
+      {children}
+    </Icon>
+  );
+}
